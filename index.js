0bb36cd4c6cfd47ae846ee80d9922796e26e8996
WallaceWyslas <wallacewyslas@gmail.com>
2024-03-29 14:30:33 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-// INPUT - VARIÁVEIS
-// Aqui se define a quantidade de Vitórias e Derrotas que o Player teve
-let saldo = quantidadeDeVitoriasDerrotas(1005, 50)
+// CLASSE
+class hero{
+    constructor(name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+    }
 
-// Função que contabiliza a diferença das Vitórias para as Derrotas
-function quantidadeDeVitoriasDerrotas(vitoria, derrota){
-    vitoriasMenosDerrotas = vitoria - derrota
-    return vitoriasMenosDerrotas
+    attack(){
+        let attackType
+        if (this.type === "Mago"){
+            attackType = "magia"
+        } else if (this.type === "Guerreiro"){
+            attackType = "espada"
+        } else if (this.type === "Monge"){
+            attackType = "artes marciais"
+        } else if (this.type === "Ninja"){
+            attackType = "shuriken"
+        }
+        let attack = attackType
+        return attack
+
+    }
+
+    print(){
+        if (this.type === "Mago" || this.type === "Guerreiro" || this.type === "Monge" || this.type === "Ninja"){
+            console.log(`o ${this.type} atacou usando ${this.attack()}!`)
+        } else{
+            console.log("Classe Inválida!")
+            console.log("Escolha entre: Mago, Guerreiro, Monge ou Ninja")
+        }
+    }
+
 }
 
-// É analisado o Nível do player
-let nivel
-if (saldo <= 10) {
-    nivel = "Ferro"
-} else if (saldo > 10 && saldo <= 20){
-    nivel = "Bronze"
-} else if (saldo > 20 && saldo <= 50){
-    nivel = "Prata"
-} else if (saldo > 50 && saldo <= 80){
-    nivel = "Ouro"
-} else if (saldo > 80 && saldo <= 90){
-    nivel = "Diamante"
-} else if (saldo > 90 && saldo <= 100){
-    nivel = "Lendário"
-} else{
-    nivel = "Imortal"
-}
+//VARIÁVEIS
+let player1 = new hero("Juremildo", 40, "Guerreiro")
 
 // SAÍDA
-console.log(`O Herói tem o saldo de ${saldo} e está no nível de ${nivel}!`)
+player1.print()
